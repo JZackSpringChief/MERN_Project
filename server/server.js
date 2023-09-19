97c7e9e6289c7e25acb95b538315acacf835e12d
Zack Spring Chief <jzacksc@gmail.com>
2023-09-19 15:40:52 -0600

    connected to mongoose successful

--- server/server.js
+++ server/server.js
@@ -1,13 +1,25 @@
 import express from "express";
 import { PORT } from "./config.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
+app.use(express.json({ extended: false }));
 
 app.get("/", (req, res) => {
-  console.log(req);
-  return res.status(200).send("MERN stack test");
+  //   console.log(req);
+  return res.status(234).send("MERN stack test");
 });
 
-app.listen(PORT, () => {
-  console.log(`APP is hearing you on ${PORT}`);
-});
+mongoose
+  .connect(process.env.mongo_DBURL)
+  .then(() => {
+    console.log("APP is connected to database");
+    app.listen(PORT, () => {
+      console.log(`APP is hearing you on ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
