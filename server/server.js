db9e5da31432ebe5895150a0686fece43b2eb90e
Zack Spring Chief <jzacksc@gmail.com>
2023-09-19 13:06:12 -0600

    setting up beginner back end

--- server/server.js
+++ server/server.js
@@ -1,3 +1,13 @@
-import Express from "express";
-
+import express from "express";
 import { PORT } from "./config.js";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  console.log(req);
+  return res.status(200).send("MERN stack test");
+});
+
+app.listen(PORT, () => {
+  console.log(`APP is hearing you on ${PORT}`);
+});
